d5f7a2b1e7652c9e81d7c3bc9b6972d971f27bfe
hemajun815 <411388997@qq.com>
2017-03-11 10:45:34 +0800

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,13 +4,13 @@
 
 int main(int argc, char const *argv[])
 {
-    WORD wVersionRequested = MAKEWORD(1,1);
+    WORD wVersionRequested = MAKEWORD(1, 1);
     WSADATA wsaData;
-    if(WSAStartup(wVersionRequested, &wsaData))
+    if (WSAStartup(wVersionRequested, &wsaData))
     {
         return -1;
     }
-    if(1 != LOBYTE(wsaData.wVersion) || 1 != HIBYTE(wsaData.wVersion))
+    if (1 != LOBYTE(wsaData.wVersion) || 1 != HIBYTE(wsaData.wVersion))
     {
         WSACleanup();
         return -1;
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
     addrServer.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
     addrServer.sin_family = AF_INET;
     addrServer.sin_port = htons(12345);
-    if(-1 != connect(sockClient, (SOCKADDR *)&addrServer, sizeof(SOCKADDR)))
+    if (-1 != connect(sockClient, (SOCKADDR *)&addrServer, sizeof(SOCKADDR)))
     {
         printf("Connect to %s.\n", inet_ntoa(addrServer.sin_addr));
     }
