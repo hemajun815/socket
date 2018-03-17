763cfe88750fe1c6eec94f3d9c7c858b774edf3b
hemajun815 <411388997@qq.com>
2018-03-17 18:58:47 +0800

    Updated python socket

--- python/client/client.py
+++ python/client/client.py
@@ -4,3 +4,11 @@
   Creator: Hemajun
   Description: Python socket client
 '''
+import socket
+
+PORT = 12345
+MAX_DATA_SIZE = 1024
+
+if __name__ == '__main__':
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.connect(('127.0.0.1', PORT))
--- python/server/server.py
+++ python/server/server.py
@@ -4,3 +4,16 @@
   Creator: Hemajun
   Description: Python socket server.
 '''
+import socket
+
+PORT = 12345
+MAX_DATA_SIZE = 1024
+
+if __name__ == '__main__':
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.bind((str(socket.INADDR_ANY), PORT))
+    sockfd.listen(8)
+    while True:
+        connfd, addr_client = sockfd.accept()
+        print connfd, addr_client
+        connfd.close()
